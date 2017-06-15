4fa6f15f9c3db912ad430360a5fc522daadf461d
Adam McCarthy <a.mccarthy.uk@googlemail.com>
2017-06-15 20:50:18 +0200

    feat: basic_white recipe finished
    Complete the first recipe. Style needs to be cleaned up.

---
+++ flourfirst/conditions.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+
+def key_conditions():
+    ''' A set of basic parameters
+
+    All units here are metric.any
+    '''
+
+    water_temp = 38
+    
---
+++ flourfirst/recipies.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python
+
+
+def basic_white():
+    ''' Recipe for basic white bread.
+
+    This is an all purpose recipe that can
+    be used to make loafs, baguette, ciabatta,
+    crust sandwich loaf and others.
+
+    Unit is metric, in grams.
+    '''
+
+    # Specify recipe input
+    water_q = 680
+    yeast_q = 10
+    salt_q = 9
+    salt_t = 'kosher'
+    flour_q = 910
+    flour_t = 'all_purpose'
+
+    # Instructions
+    Step_1 = (
+    '1. Warm the 3 cups water just a little so that'
+    'it feels just slightly warmer than body temperature.' 
+    'That should put it at about 100°F (40°C).' 
+    'In the large bowl of a standing mixer or a 6-quart' 
+    'container with a lid, mix the yeast, warm water, and salt.' 
+    'Don’t worry about getting the yeast to dissolve.' 
+    'Add the flour all at once, then use a spoon or stand mixer' 
+    'to mix until the flour is completely incorporated and you' 
+    'have a blobby dough. (If you’re hand-mixing the dough and' 
+    'it becomes too difficult to incorporate all the flour with' 
+    'the spoon, just use very wet hands to press the mixture together.)' 
+    'Don’t knead the dough! It’s not necessary. You just want the' 
+    'dough to be uniformly wet and loose enough to conform to the' 
+    'shape of its container. All you need to do is be certain that' 
+    'there are no dry patches of flour.'
+    )
+
+    Step_2 = (
+    '2. Loosely cover the container and let the dough hang out at'
+    'room temperature until it begins to rise and collapse or at' 
+    'least flatten a little on the top, about 2 hours.' 
+    '(Relax. It’s bread dough, not a newborn.'
+    'You don’t need to monitor it constantly.' 
+    'And don’t worry about the dough being precisely double or triple' 
+    'its original volume as you would with a traditional bread recipe.' 
+    'Just walk away, go about your business, and come back in 2 hours. Seriously.)'
+    )
+
+    Step_3 = (
+    '3. After 2 hours, stash the container of dough in the fridge.' 
+    'That’s it. (If your container isn’t vented, you want to ensure' 
+    'the gases can escape by leaving the cover open a crack for the' 
+    'first couple days in the fridge; after that, you can seal it.)' 
+    'You can use the dough anytime after the initial 2-hour rise,' 
+    'although the refrigerated wet dough is less sticky and easier' 
+    'to work with than dough at room temperature, so it’s best to' 
+    'refrigerate the dough overnight before handling it.' 
+    'Once refrigerated, the dough will seem to have shrunk back' 
+    'upon itself as though it will never rise again—that’s normal.' 
+    'Whatever you do, do not punch down this dough.' 
+    'You’re trying to retain as much gas in the dough as possible,' 
+    'and punching it down knocks gas out and results in denser loaves.' 
+    'Just be certain to use the dough at some point within 14 days.'
+    )
