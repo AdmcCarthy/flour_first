b3b5d9ee52b33c779effdc0c560ef71917af2566
Adam McCarthy <a.mccarthy.uk@googlemail.com>
2017-06-15 21:47:59 +0200

    feat: Created cooking module and first instructions
    Created a module along wiht the artisian_bread function.

---
+++ flourfirst/cooking.py
@@ -0,0 +1,83 @@
+#!/usr/bin/python
+
+
+def artisian_bread():
+    '''Method to bake any of the
+    artisian bread recipes.
+    '''
+    
+    Step_1 = (
+       'When you want to bake a loaf of artisan bread,' 
+       'dust a pizza peel or a baking sheet turned upside'
+       'down with cornmeal or line it with parchment paper.' 
+       'Grab a hunk of the dough and use a serrated knife' 
+       'or scissors to cut off a 1-pound piece of dough.' 
+       'Hold the dough in your hands and, if necessary,' 
+       'add just enough flour so the dough doesn’t stick' 
+       'to your hands. (What you’re trying to do is surround' 
+       'the surface of the dough with flour so that it can' 
+       'be handled. You are not trying to incorporate more' 
+       'flour into the dough, so for the love of all things' 
+       'good, resist the temptation to get rid of all the' 
+       'dough’s inherent and lovely stickiness by working' 
+       'the flour into the dough.)' 
+       'Gently stretch the surface of the dough,' 
+       'tucking the ends underneath the ball and rotating it' 
+       'a quarter turn as you go. Most of the dusting flour' 
+       'will fall off, and that’s okay, because as we just said,' 
+       'it’s not intended to be incorporated into the dough.' 
+       'The bottom of the ball of dough may appear to be a' 
+       'collection of bunched ends, but it will flatten out' 
+       'and adhere during resting and baking.' 
+       'Your round loaf of bread should be smooth and cohesive,' 
+       'and the entire shaping process should take no more than' 
+       '20 to 40 seconds—don’t work the dough any longer or your' 
+       'loaves may be dense. Place the shaped ball of dough on' 
+       'the prepared pizza peel and let it rest for about 40 minutes.' 
+       'It doesn’t need to be covered.' 
+       'You may not see much rise during this period, but don’t fret.' 
+       'It will rise much more during baking.'
+        )
+
+    Step_2 = (
+        'Preheat the oven to 450°F (230°C) for at least' 
+        '20 to 30 minutes.' 
+        'Preheat a baking stone on a middle rack for' 
+        'at least 20 to 30 minutes.' 
+        'Place an empty metal broiler tray on any rack' 
+        'that won’t interfere with the rising bread.' 
+        '(Do not use a glass pan as it could shatter.)'
+        )
+    
+    Step_3 = (
+        'Dust the top of the raised loaf generously with' 
+        'flour and, using a serrated bread knife,' 
+        'slash a 1/2-inch-deep cross or tic-tac-toe pattern' 
+        'in the top.' 
+        'There’s no need to dust the flour off the loaf.'
+        )
+
+    Step_4 = (
+        'Place the far edge of the peel or the upside-down' 
+        'baking sheet in the oven on the baking stone a' 
+        'few inches beyond where you want the bread to land.' 
+        'Give the peel or baking sheet a couple' 
+        'quick back-and-forth jiggles and then abruptly pull' 
+        'it out from under the loaf.' 
+        'The loaf should land on the baking stone with' 
+        'very little drama.' 
+        'Quickly but carefully pour about 1 cup hot water' 
+        'into the broiler tray and immediately shut' 
+        'the oven door to trap the steam.' 
+        'Bake the bread for a total of 20 to 35 minutes,' 
+        'until the crust is richly browned and firm to the touch.' 
+        '(Don’t worry. Because the dough is so wet,' 
+        'there’s very little risk of it becoming dry' 
+        'despite how dark the crust may become.)' 
+        'And crazily enough, a perfectly baked loaf will' 
+        'audibly crackle, or “sing,” when initially exposed' 
+        'to room temperature. Let the loaf cool completely,' 
+        'preferably on a wire rack for the best flavor,' 
+        'texture, and slicing.' 
+        'The crust may initially soften but will firm when cooled.'
+        )
